2cbc347101a306b5379a354e319fa7f813793fc2
Matt Morrisey <mattydevicce@gmail.com>
2015-11-17 16:14:38 -0500

    made links open in a new tab

---
+++ java.js
@@ -0,0 +1,21 @@
+console.log('javascript working')
+
+image1="./images/quizapp.png"
+image2="./images/quizapp2.png"
+image3="./images/quizapp3.png"
+
+var step = 2
+var killswitch
+$(".project1").hover(function() {
+	killswitch = setInterval(function(){
+		$("#slide").attr("src", eval("image"+step))
+		if (step<3){
+			step++
+		} else {
+			step=1
+		}
+	}, 2000)
+},
+function() {
+	clearInterval(killswitch);
+})
